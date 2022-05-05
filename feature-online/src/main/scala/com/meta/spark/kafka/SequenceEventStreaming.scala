fe55f6c94bba31384e06065710d0b1e4c6c70406
weitaoliang <weitaoliang@tencent.com>
2022-05-05 15:39:47 +0800

    [fix]:单流序列特征处理

---
+++ feature-online/src/main/scala/com/meta/spark/kafka/SequenceEventStreaming.scala
@@ -0,0 +1,35 @@
+package com.meta.spark.kafka
+
+import com.meta.conn.redis.JedisClusterName
+import com.meta.entity.FeatureTypeEnum.FeatureTypeEnum
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.DStream
+
+/**
+ * 单条序列特征处理类
+ *
+ * idFunctionArray Array(field,通过id获取关联id的方法，多用于交叉特征，保存最大长度，保存最大时长) 建议保存时长不超过2天
+ * parseStream 解析每行topic 事件(topic,line) 转化成 （唯一标识，Array(事件事件，事件标识))
+ *
+ * @author: weitaoliang
+ * @version v1.0
+ * */
+class SequenceEventStreaming(spark: SparkSession,
+                             kafkaSource: KafkaSourceStreaming,
+                             redisKeyPattern: String,
+                             jedisClusterName: JedisClusterName,
+                             idFunctionArray: Array[(String, String => String, Int, Int)],
+                             parseStream:
+                             DStream[(String, String)] => DStream[(String, Array[(Long, String)])]
+                            ) extends Serializable {
+  private var _featureType: FeatureTypeEnum = _
+
+  def run(): Unit = {
+    val multiSequenceEventStreaming =
+      new MultiSequenceEventStreaming(spark, kafkaSource, jedisClusterName)
+    val events = parseStream(multiSequenceEventStreaming.kafkaStream).filter(_ != null)
+    assert(_featureType == null, "必须设置featureType!!!")
+    multiSequenceEventStreaming.putEvents(redisKeyPattern, _featureType, events, idFunctionArray)
+  }
+
+}

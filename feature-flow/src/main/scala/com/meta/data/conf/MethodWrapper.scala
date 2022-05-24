335eac82092e3b48798953c8b11174d5b159118e
weitaoliang <weitaoliang@tencent.com>
2022-05-24 17:16:53 +0800

    [feat]:新增机器学习流水线特征处理配置类

---
+++ feature-flow/src/main/scala/com/meta/data/conf/MethodWrapper.scala
@@ -0,0 +1,46 @@
+package com.meta.data.conf
+
+/**
+ * 特征处理方法包装类
+ *
+ * @author: weitaoliang
+ * @version v1.0
+ * */
+class MethodWrapper(name: String,
+                    f: AnyRef,
+                    val alias: String) extends Serializable {
+  def eval(params: Array[Any]): Any = {
+    params.length match {
+      case 0 =>
+        val func = f.asInstanceOf[() => Any]
+        func()
+      case 1 =>
+        val func = f.asInstanceOf[(Any) => Any]
+        func(params(0))
+      case 2 =>
+        val func = f.asInstanceOf[(Any, Any) => Any]
+        func(params(0), params(1))
+      case 3 =>
+        val func = f.asInstanceOf[(Any, Any, Any) => Any]
+        func(params(0), params(1), params(2))
+      case 4 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3))
+      case 5 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3), params(4))
+      case 6 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3), params(4), params(5))
+      case 7 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3), params(4), params(5), params(6))
+      case 8 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any, Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3), params(4), params(5), params(6), params(7))
+      case 9 =>
+        val func = f.asInstanceOf[(Any, Any, Any, Any, Any, Any, Any, Any, Any) => Any]
+        func(params(0), params(1), params(2), params(3), params(4), params(5), params(6), params(7), params(8))
+    }
+  }
+}
